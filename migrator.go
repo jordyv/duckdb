9698c15fbecbf0400507fdf4a9190c6d60395739
Ali Firoozi Zamani <ali.firoozizamani@arvancloud.ir>
2024-10-28 12:45:33 +0330

    chore(migrator): Add column operations

--- migrator.go
+++ migrator.go
@@ -1,6 +1,8 @@
 package duckdb
 
 import (
+	"errors"
+	"fmt"
 	"strings"
 
 	"gorm.io/gorm"
@@ -97,24 +99,6 @@ func (m Migrator) HasTable(value interface{}) bool {
 	return count > 0
 }
 
-func (m Migrator) RenameColumn(dst interface{}, oldName, field string) error {
-	if err := m.Migrator.RenameColumn(dst, oldName, field); err != nil {
-		return err
-	}
-
-	m.resetPreparedStmts()
-	return nil
-}
-
-// should reset prepared stmts when table changed
-func (m Migrator) resetPreparedStmts() {
-	if m.DB.PrepareStmt {
-		if pdb, ok := m.DB.ConnPool.(*gorm.PreparedStmtDB); ok {
-			pdb.Reset()
-		}
-	}
-}
-
 func (m Migrator) RenameTable(oldName, newName interface{}) (err error) {
 	resolveTable := func(name interface{}) (result string, err error) {
 		if v, ok := name.(string); ok {
@@ -153,13 +137,122 @@ func (m Migrator) GetTables() (tableList []string, err error) {
 	return tableList, m.DB.Raw("SELECT table_name FROM information_schema.tables WHERE table_schema = ? AND table_type = ?", currentSchema, "BASE TABLE").Row().Scan(&tableList)
 }
 
-// // Columns
-// AddColumn(dst interface{}, field string) error
-// DropColumn(dst interface{}, field string) error
+// Columns
+
+func (m Migrator) AddColumn(value interface{}, field string) error {
+	return m.RunWithValue(value, func(stmt *gorm.Statement) error {
+		if f := stmt.Schema.LookUpField(field); f != nil {
+			// avoid using the same name field
+			if stmt.Schema == nil {
+				return errors.New("failed to get schema")
+			}
+			f := stmt.Schema.LookUpField(field)
+			if f == nil {
+				return fmt.Errorf("failed to look up field with name: %s", field)
+			}
+			if !f.IgnoreMigration {
+				return m.DB.Exec(
+					"ALTER TABLE ? %s ADD COLUMN ? ?",
+					m.CurrentTable(stmt), clause.Column{Name: f.DBName}, m.DB.Migrator().FullDataTypeOf(f),
+				).Error
+			}
+			if f.Comment != "" {
+				if err := m.DB.Exec(
+					"COMMENT ON COLUMN ?.? IS ?",
+					m.CurrentTable(stmt), clause.Column{Name: f.DBName}, gorm.Expr(m.Migrator.Dialector.Explain("$1", f.Comment)),
+				).Error; err != nil {
+					return err
+				}
+			}
+		}
+		return nil
+	})
+}
+
+func (m Migrator) DropColumn(dst interface{}, field string) error {
+	if err := m.Migrator.DropColumn(dst, field); err != nil {
+		return err
+	}
+
+	m.resetPreparedStmts()
+	return nil
+}
+
+// should reset prepared stmts when table changed
+// https://duckdb.org/docs/sql/query_syntax/prepared_statements.html
+func (m Migrator) resetPreparedStmts() {
+	if m.DB.PrepareStmt {
+		if pdb, ok := m.DB.ConnPool.(*gorm.PreparedStmtDB); ok {
+			pdb.Reset()
+		}
+	}
+}
+
+func (m Migrator) MigrateColumn(value interface{}, field *schema.Field, columnType gorm.ColumnType) error {
+	// skip primary field
+	if !field.PrimaryKey {
+		if err := m.Migrator.MigrateColumn(value, field, columnType); err != nil {
+			return err
+		}
+	}
+
+	return m.RunWithValue(value, func(stmt *gorm.Statement) error {
+		var description string
+		currentSchema, curTable := m.CurrentSchema(stmt, stmt.Table)
+		values := []interface{}{currentSchema, curTable, field.DBName, stmt.Table, currentSchema}
+		checkSQL := "SELECT description FROM pg_catalog.pg_description "
+		checkSQL += "WHERE objsubid = (SELECT ordinal_position FROM information_schema.columns WHERE table_schema = ? AND table_name = ? AND column_name = ?) "
+		checkSQL += "AND objoid = (SELECT oid FROM pg_catalog.pg_class WHERE relname = ? AND relnamespace = "
+		checkSQL += "(SELECT oid FROM pg_catalog.pg_namespace WHERE nspname = ?))"
+		m.DB.Raw(checkSQL, values...).Row().Scan(&description)
+
+		comment := strings.Trim(field.Comment, "'")
+		comment = strings.Trim(comment, `"`)
+		if field.Comment != "" && comment != description {
+			if err := m.DB.Exec(
+				"COMMENT ON COLUMN ?.? IS ?",
+				m.CurrentTable(stmt), clause.Column{Name: field.DBName}, gorm.Expr(m.Migrator.Dialector.Explain("$1", field.Comment)),
+			).Error; err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+}
+
+// TODO: Implement below function.
 // AlterColumn(dst interface{}, field string) error
-// MigrateColumn(dst interface{}, field *schema.Field, columnType ColumnType) error
-// HasColumn(dst interface{}, field string) bool
-// RenameColumn(dst interface{}, oldName, field string) error
+
+func (m Migrator) HasColumn(value interface{}, field string) bool {
+	var count int64
+	m.RunWithValue(value, func(stmt *gorm.Statement) error {
+		name := field
+		if stmt.Schema != nil {
+			if field := stmt.Schema.LookUpField(field); field != nil {
+				name = field.DBName
+			}
+		}
+
+		currentSchema, curTable := m.CurrentSchema(stmt, stmt.Table)
+		return m.DB.Raw(
+			"SELECT count(*) FROM INFORMATION_SCHEMA.columns WHERE table_schema = ? AND table_name = ? AND column_name = ?",
+			currentSchema, curTable, name,
+		).Scan(&count).Error
+	})
+
+	return count > 0
+}
+
+func (m Migrator) RenameColumn(dst interface{}, oldName, field string) error {
+	if err := m.Migrator.RenameColumn(dst, oldName, field); err != nil {
+		return err
+	}
+
+	m.resetPreparedStmts()
+	return nil
+}
+
+// TODO: Implement below function.
 // ColumnTypes(dst interface{}) ([]ColumnType, error)
 
 // // Views
